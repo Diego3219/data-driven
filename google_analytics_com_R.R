80d8cb071a1caa0fc199b9a51c972d1c7592ac4b
Diego3219 <diegoribeiro3219@gmail.com>
2017-09-29 15:26:25 -0300

    ajustes para o TDC

--- google_analytics_com_R.R
+++ google_analytics_com_R.R
@@ -1,34 +1,36 @@
-# Documentação Oficial
+# Documenta??o Oficial
 #https://developers.google.com/analytics/solutions/r-google-analytics
 
 # Instalando e ativando o pacote do Google Analytics para o R
 
-#Aplicando uma camada de Ciência no Marketing Digital
+#Aplicando uma camada de Ci?ncia no Marketing Digital
 
 # Instalando o Pacote
+library(needs)
 needs(RGoogleAnalytics)
 
 # Autorizar a conta do Google Analytics
-# Isso não precisa ser executado em cada sessão uma vez que o objeto token criado
-# é salvo
+# Isso n?o precisa ser executado em cada sess?o uma vez que o objeto token criado
+# ? salvo
 
-# Substitua as chaves pelas que você gerou no projeto da API
+# Substitua as chaves pelas que voc? gerou no projeto da API
 #https://console.developers.google.com
-client.id <-"xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com"
-client.secret <-"xxxxxxxxxxxxxxxxxxxxxxxx"
+client.id <-"xx"
+client.secret <-"xxx"
 token <- Auth(client.id,client.secret)
 
-# Salvar o objeto token para sessões futuras
+# Salvar o objeto token para sess?es futuras
 save(token,file="./token_file")
 
-# Em sessões futuras, ele pode ser carregado 
+# Em sess?es futuras, ele pode ser carregado 
 load("./token_file")
 
 ValidateToken(token)
+GetProfiles(token)
 
-# Criar uma lista de consulta com os parâmetros da API
+# Criar uma lista de consulta com os par?metros da API
 query.list <- Init(start.date = "2017-01-01",
-                   end.date = "2016-01-31",
+                   end.date = "2017-08-31",
                    dimensions = "ga:date, ga:dayOfWeek, ga:source, ga:medium, ga:campaign, ga:deviceCategory",
                    metrics = "
                    ga:sessions,
@@ -43,30 +45,30 @@ query.list <- Init(start.date = "2017-01-01",
                    ga:totalValue",
                    max.results = 10000,
                    sort = "ga:date",
-                   table.id = "ga:xxxxxxxx")
+                   table.id = "ga:xxxxxxx")
                    
 
 
-# Crie o objeto Query Builder para que os parâmetros de consulta sejam validados
+# Crie o objeto Query Builder para que os par?metros de consulta sejam validados
 ga.query <- QueryBuilder(query.list)
 
-# Extrair os dados e armazená-los em um data-frame
+# Extrair os dados e armazen?-los em um data-frame
 ga.data <- GetReportData(ga.query, token, paginate_query = F)
 head(ga.data)
 
-#Gravar os dados em outra variável é uma boa prática, pois em caso de neccessidade,
-#os dados originais continuam armazenados e disponíveis.
+#Gravar os dados em outra vari?vel ? uma boa pr?tica, pois em caso de neccessidade,
+#os dados originais continuam armazenados e dispon?veis.
 analytics <- data.frame(ga.data)
 head(analytics)
 
 #explorando a estrutura dos dados
 str(analytics)
 
-#os dados virão brutos e em formatos que não permitem as análises, portanto é preciso transformá-los!
+#os dados vir?o brutos e em formatos que n?o permitem as an?lises, portanto ? preciso transform?-los!
 
 #transformando a primeira coluna em Data
 class(analytics$date)
-#pode ser necessário instalar o pacote antes de utilizá-lo
+#pode ser necess?rio instalar o pacote antes de utiliz?-lo
 needs(lubridate)
 analytics$date <- ymd(analytics$date)
 class(analytics$date)
@@ -75,36 +77,36 @@ any(is.na(analytics))
 
 # Criar um fator ordenado para o dia da semana
 analytics$dayOfWeek <- as.numeric(analytics$dayOfWeek)
-analytics$dayOfWeek <- factor(analytics$dayOfWeek, labels = c("Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado"))
+analytics$dayOfWeek <- factor(analytics$dayOfWeek, labels = c("Domingo","Segunda","Ter?a","Quarta","Quinta","Sexta","S?bado"))
 
-#arredondar as colunas que representam valor monetário
+#arredondar as colunas que representam valor monet?rio
 analytics$CPC <- round(analytics$CPC,2)
 analytics$adCost <- round(analytics$adCost,2)
 analytics$costPerTransaction <- round(analytics$costPerTransaction,2)
 analytics$totalValue <- round(analytics$totalValue,2)
 str(analytics)
 
-# Aqui começa a construção do Modelo Predittivo
-# Análise exploratória dos dados
+# Aqui come?a a constru??o do Modelo Predittivo
+# An?lise explorat?ria dos dados
 
-# Pequenas Análises
+# Pequenas An?lises
 dispositivo <- table(analytics$deviceCategory)
 (dispositivo <- round((prop.table(dispositivo) * 100)))
 dia <-  table(analytics$dayOfWeek)
 (dia <- round(prop.table(dia) * 100))
 
-# Construção de alguns gráficos para análises visuais
-# Transações x potenciais variaveis 
-# Pode ser necessário instalar o pacote
+# Constru??o de alguns gr?ficos para an?lises visuais
+# Transa??es x potenciais variaveis 
+# Pode ser necess?rio instalar o pacote
 needs(ggplot2)
-labels <- list("Boxplots - Transações por Origem",
-               "Boxplots - Transações por Mídia",
-               "Boxplots - Transações por Dia Dispositivo",
-               "Boxplots - Transações por Dia da Semana")
+labels <- list("Boxplots - Transa??es por Origem",
+               "Boxplots - Transa??es por M?dia",
+               "Boxplots - Transa??es por Dia Dispositivo",
+               "Boxplots - Transa??es por Dia da Semana")
 
 xAxis <- list("source", "medium", "deviceCategory", "dayOfWeek")
 
-# Gráficos
+# Gr?ficos
 plot.scatter <- function(X, label){ 
   ggplot(analytics, aes_string(x = X, y = "totalValue")) + 
     geom_point(aes_string(colour = "totalValue"), alpha = 0.1) + 
@@ -115,37 +117,37 @@ plot.scatter <- function(X, label){
 
 Map(plot.scatter, xAxis, labels)
 
-# Obtendo apenas as colunas numéricas
-# Pode ser necessário instalar o pacote
+# Obtendo apenas as colunas num?ricas
+# Pode ser necess?rio instalar o pacote
 needs(dplyr)
 analytics_numericas <- sapply(analytics,is.numeric)
 analytics <- analytics[, analytics_numericas]
 
-#Análise de Correlação
+#An?lise de Correla??o
 
 par(mfrow=c(1,1))
 data_cor <- cor(analytics)
 data_cor
-# Pode ser necessário instalar o pacote
+# Pode ser necess?rio instalar o pacote
 needs(corrplot)
-# Pode ser necessário instalar o pacote
+# Pode ser necess?rio instalar o pacote
 needs(corrgram)
 corrplot(data_cor, method = 'circle')
-# Alternativas de gráficos
+# Alternativas de gr?ficos
 #corrgram(analytics)
 #corrgram(analytics, order = T, lower.panel = panel.shade, 
 #         upper.panel = panel.pie, text.panel = panel.txt)
 #pairs(data_cor, panel = panel.smooth)
 
-# Alternativa de Regressão Simples
+# Alternativa de Regress?o Simples
 
-# Selecionando as váriaveis
+# Selecionando as v?riaveis
 a <- analytics$impressions
 b <- analytics$totalValue
 c <- data.frame(a,b)
 
 # Dividindo os dados para o modelo
-# Pode ser necessário instalar o pacote
+# Pode ser necess?rio instalar o pacote
 needs(caTools)
 set.seed(100)
 amostra <- sample.split(c$a, SplitRatio = 0.70)
@@ -153,24 +155,30 @@ treino = subset(c, amostra = T)
 teste = subset(c, amostra = F)
 
 
-# Construindo o modelo de regressão
+# Construindo o modelo de regress?o
 modelo <- lm(b ~ a, treino)
 summary(modelo)
 #plot(modelo)
 
-# Plotando o gráfico de dispersão com a reta de regressão
+# Plotando o gr?fico de dispers?o com a reta de regress?o
 k <- coefficients(modelo)
 plot(a,b)
 abline(k)
 
-#fazendo as predições
+#fazendo as predi??es
 #modelo <- lm(b ~ a, treino)
 prevendo_totalValue <- predict(modelo,teste)
 sum(prevendo_totalValue)
 
 # Alterando o Valor para responder a pergunta do problema formulado
-venda_previsto <- data.frame(x=20000)
-previsao_1 <- predict(modelo, venda_previsto)
-sum(previsao_1)
+#venda_previsto <- data.frame(x=20000)
+#y = ax + b
+(venda_previsto <- k[1]*35000 + k[2])
+(sum(analytics$totalValue))
+(sum(analytics$adCost))
+#previsao_1 <- predict(modelo, venda_previsto)
+#sum(previsao_1)
 
-# Este Modelo está sofrendo de overfiting e precisa ser ajustado
+
+
+# Este Modelo est? sofrendo de overfiting e precisa ser ajustado
